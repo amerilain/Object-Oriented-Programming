45fe3e2623ae8c88b63ef04675d8ac7468eff0a0
amerilain <amerilain@gmail.com>
2024-03-12 11:31:03 +0200

    works

--- 11e/main.cpp
+++ 11e/main.cpp
@@ -1,5 +1,4 @@
 #include <cstdlib>
-
 #include <iostream>
 #include <memory>
 #include <utility>
@@ -28,8 +27,6 @@ private:
 
 void Car::Read()
 {
-    // std::cout << "Enter car information" << std::endl;
-    // replace the following with your own code
     model = "Seat";
     license = (rand() % 2 ? "ZAP-" : "ZIP-") + std::to_string(rand() % 999 + 1);
     mileage = 10000 + rand() % 10000;
@@ -38,7 +35,6 @@ void Car::Read()
 std::ostream & operator<<(std::ostream & out, const Car & car)
 {
     std::cout << "Model: " << car.model << std::endl << "License: " << car.license << std::endl << "Mileage: " << car.mileage << std::endl;
-
     return out;
 }
 
@@ -48,15 +44,21 @@ class Website {
 public:
     explicit Website(std::string n="carweb.com") : name(std::move(n)) {}
     ~Website() { std::cout << name << " deleted" << std::endl; };
-    void add(Car *car, std::string &dealer) { listing[car] = dealer; }
+    void add(std::shared_ptr<Car> car, std::string &dealer) { listing[car] = dealer; }
     void print(std::ostream &out = std::cout) {
         out << name << std::endl;
-        for(auto [car, dealer] : listing) out << dealer << ":" << std::endl << *car;
+        for(auto it = listing.begin(); it != listing.end(); ) {
+            if(it->first.expired()) {
+                it = listing.erase(it);
+            } else {
+                out << it->second << ":" << std::endl << *(it->first.lock());
+                ++it;
+            }
+        }
         out << name << " end of list" << std::endl;
     }
-    void remove(Car *car) { listing.erase(car); }
 private:
-    std::map<Car *, std::string> listing;
+    std::map<std::weak_ptr<Car>, std::string, std::owner_less<std::weak_ptr<Car>>> listing;
     std::string name;
 };
 
@@ -67,22 +69,22 @@ public:
     ~Dealer() { std::cout << name << " deleted" << std::endl; };
     void buy();
     void sell();
-    void add(Car *car) {
+    void add(std::shared_ptr<Car> car) {
         cars.push_back(car);
         for (auto site : sites) site->add(car, name);
     }
-    void add_site(Website *w) {
+    void add_site(std::shared_ptr<Website> w) {
         sites.push_back(w);
     }
 private:
     std::string name;
-    std::vector<Car *> cars;
-    std::vector<Website *> sites;
+    std::vector<std::shared_ptr<Car>> cars;
+    std::vector<std::shared_ptr<Website>> sites;
 };
 
 void Dealer::buy()
 {
-    Car *car = new Car;
+    std::shared_ptr<Car> car = std::make_shared<Car>();
     car->Read();
     add(car);
 }
@@ -94,40 +96,33 @@ void Dealer::sell()
 
     std::string license;
     std::cin >> license;
-    auto ci = std::find_if(cars.begin(), cars.end(), [&license](Car *c) {return license == c->GetLicense(); });
+    auto ci = std::find_if(cars.begin(), cars.end(), [&license](std::shared_ptr<Car> &c) {return license == c->GetLicense(); });
     if (ci != cars.end()) {
-        // modify code so that you don't need to remove a sold car from the website
-        for (auto site : sites) site->remove(*ci);
         cars.erase(ci);
     }
 }
 
-
 std::ostream & operator<<(std::ostream & out, const Dealer & dealer)
 {
     std::cout << dealer.name << "'s cars for sale" << std::endl;
-    for (auto car : dealer.cars) std::cout << *car;
+    for (auto &car : dealer.cars) std::cout << *car;
     std::cout << "End of " << dealer.name << "'s cars listing" << std::endl;
 
     return out;
 }
 
-/* ---------------------- */
-
-
-
 void car_sales()
 {
     std::cout << "Car sales started" << std::endl;
-    auto *wa = new Website("www.autos.com");
-    auto *wb = new Website("www.bilar.com");
-    auto *a = new Dealer("Alan Aldis");
-    auto *b = new Dealer("Bill Munny");
+    auto wa = std::make_shared<Website>("www.autos.com");
+    auto wb = std::make_shared<Website>("www.bilar.com");
+    auto a = std::make_shared<Dealer>("Alan Aldis");
+    auto b = std::make_shared<Dealer>("Bill Munny");
     { // inner scope to make some if the pointers go out of scope before the function ends.
-        auto *wc = new Website("www.cars.com");
-        auto *c = new Dealer("Casey Ball");
-        Car *ca = new Car;
-        Car *cb = new Car;
+        auto wc = std::make_shared<Website>("www.cars.com");
+        auto c = std::make_shared<Dealer>("Casey Ball");
+        auto ca = std::make_shared<Car>();
+        auto cb = std::make_shared<Car>();
 
         a->add_site(wa);
         a->add_site(wb);
@@ -169,18 +164,13 @@ void car_sales()
     wa->print();
     wb->print();
 
-
     std::cout << "Car sales ended" << std::endl;
-
 }
 
 int main(int argc, char **argv) {
 
     srand(time(nullptr));
-
     car_sales();
-
     return 0;
 }
-
 
